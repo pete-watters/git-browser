527bba0c1d017e0d2acef915a0ea9d3fb51fc611
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-01-08 11:47:47 +0000

    Add star repo action

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(){
         return this.store.findRecord('user', 'peter-watters');
+    },
+    actions: {
+        star(repo) {
+            repo.save();
+        }
     }
 });
