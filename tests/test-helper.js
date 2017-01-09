2a3f30989b4b72f2d2ffffcdaa2eb62c6404177d
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-01-09 19:25:07 +0000

    Remove workaround

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,14 +2,3 @@ import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
 
 setResolver(resolver);
-
-if (window.location.search.indexOf('nocontainer') > -1) {
-    Ember.$('#ember-testing-container').css({
-        visibility: 'hidden'
-    });
-}
-
-Ember.$('body').on('click.mocha-stats', '#mocha-stats a', function (e) {
-    e.preventDefault();
-    return false;
-});
