01812e54a0fabe05e086b7459a50de83cc11d209
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-01-09 23:05:47 +0000

    Always clear token from localstorage on route load

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 export default Ember.Route.extend( {
+  beforeModel(){
+    // remove key from localStorage if exists
+    window.localStorage.removeItem('gitHubAccessToken');
+
+  },
     model(){
         const store = this.store;
         const organisation = this.get('organisationSearchString');
