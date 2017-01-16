f53f158b708aff9fabcda37f19628787c9c3039e
Peter Watters <peterwatters@MacBook-Pro-2.local>
2017-01-16 17:34:06 +0000

    Rollback config change

--- config/environment.js
+++ config/environment.js
@@ -8,9 +8,7 @@ module.exports = function(environment) {
     locationType: 'auto',
 
     EmberENV: {
-      EXTEND_PROTOTYPES: {
-        Date: false,
-      },
+
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
