da24f87f0a7a91ebb9f715421ec013c2e63363da
Peter Watters <peter.j.watters@gmail.com>
2017-01-16 08:51:33 +0000

    Fix search filter

--- app/components/repository-list.js
+++ app/components/repository-list.js
@@ -1,29 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement: function() {
+    filteredRepositoryList: function() {
+      var filteredModel;
+      console.log('filtered');
+      if(this.get('repositoryLanguageSearchString') && this.get('repositoryPrivateFlag')){
 
+        filteredModel = this.get('inputModel').filterBy('language', this.get('repositoryLanguageSearchString'));
+        filteredModel = filteredModel.filterBy('private', this.get('repositoryPrivateFlag'));
+      }else if (this.get('repositoryLanguageSearchString')){
+         filteredModel = this.get('inputModel').filterBy('language', this.get('repositoryLanguageSearchString'));
+      }else if(this.get('repositoryPrivateFlag')){
+        filteredModel = this.get('inputModel').filterBy('private', this.get('repositoryPrivateFlag'));
+      } else{
+        filteredModel = this.get('inputModel');
+      }
+      return filteredModel;
+    }.property('repositoryLanguageSearchString', 'repositoryPrivateFlag'),
+
+    didInsertElement: function() {
     Ember.$('#repository-list-0').addClass('is-active');
-    Ember.run.scheduleOnce('afterRender', this, function() {
+  },
+  didRender: function (){
+    Ember.$(".js-vertical-tab-content").hide();
+    Ember.$(".js-vertical-tab-content:first").show();
+
+    Ember.$(".js-vertical-tab").click(function(event) {
+      event.preventDefault();
 
       Ember.$(".js-vertical-tab-content").hide();
-      Ember.$(".js-vertical-tab-content:first").show();
+      var activeTab = Ember.$(this).attr("rel");
+      Ember.$("#"+activeTab).show();
 
-      /* if in tab mode */
-      Ember.$(".js-vertical-tab").click(function(event) {
-        event.preventDefault();
+      Ember.$(".js-vertical-tab").removeClass("is-active");
+      Ember.$(this).addClass("is-active");
 
-        Ember.$(".js-vertical-tab-content").hide();
-        var activeTab = Ember.$(this).attr("rel");
-        Ember.$("#"+activeTab).show();
-
-        Ember.$(".js-vertical-tab").removeClass("is-active");
-        Ember.$(this).addClass("is-active");
-
-        Ember.$(".js-vertical-tab-accordion-heading").removeClass("is-active");
-        Ember.$(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
-      });
+      Ember.$(".js-vertical-tab-accordion-heading").removeClass("is-active");
+      Ember.$(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
     });
-  }
+  },
 
 });
--- tests/integration/components/repository-detail-test.js
+++ tests/integration/components/repository-detail-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('search-filters', 'Integration | Component | search filters', {
+moduleForComponent('repository-detail', 'Integration | Component | repository detail', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{search-filters}}`);
+  this.render(hbs`{{repository-detail}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#search-filters}}
+    {{#repository-detail}}
       template block text
-    {{/search-filters}}
+    {{/repository-detail}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
